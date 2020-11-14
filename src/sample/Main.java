ebd80e686e5d9d4f9f7e4bb5518ca195a8b45f7f
AlexAndrew28 <60033885+AlexAndrew28@users.noreply.github.com>
2020-11-14 13:05:20 +0000

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,39 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Main menu");
+
+        Button button_endless = new Button("Endless Mode");
+        Button button_campaign = new Button("Campaign mode");
+        Button button_inventory = new Button("Inventory");
+        Button button_shop = new Button("Shop");
+
+        button_shop.setOnAction(value ->  {
+            System.out.println("button - shop");
+        });
+
+        GridPane mmgp = new GridPane();
+
+        mmgp.add(button_endless, 0, 0, 1, 1);
+        mmgp.add(button_campaign, 0, 1, 1, 1);
+        mmgp.add(button_inventory, 0, 2, 1, 1);
+        mmgp.add(button_shop, 0, 3, 1, 1);
+
+        Scene scene = new Scene(mmgp, 200, 100);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
