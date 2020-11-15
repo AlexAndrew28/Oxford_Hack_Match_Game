37ebc9184342ccf3417778930786f8ac39773d99
Kieran Kohtz <kohtzk@gmail.com>
2020-11-15 06:50:33 +0000

    finished basic functionality of swap game

--- src/sample/SceneTest.java
+++ src/sample/SceneTest.java
@@ -1,10 +1,18 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -13,6 +21,7 @@ import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.util.Random;
 
@@ -24,8 +33,60 @@ public class SceneTest extends Application {
         primaryStage.setScene(new GameScreen().getScene());
         primaryStage.show();
 //        primaryStage.setFullScreen(true);
-
+        System.out.println();
     }
+
+//    public static final double W = 200; // canvas dimensions.
+//    public static final double H = 200;
+//
+//    public static final double D = 20;  // diameter.
+
+//    @Override public void start(Stage stage) {
+//        DoubleProperty x  = new SimpleDoubleProperty();
+//        DoubleProperty y  = new SimpleDoubleProperty();
+//
+//        Timeline timeline = new Timeline(
+//                new KeyFrame(Duration.seconds(0),
+//                        new KeyValue(x, 0),
+//                        new KeyValue(y, 0)
+//                ),
+//                new KeyFrame(Duration.seconds(3),
+//                        new KeyValue(x, W - D),
+//                        new KeyValue(y, H - D)
+//                )
+//        );
+//        timeline.setAutoReverse(true);
+//        timeline.setCycleCount(Timeline.INDEFINITE);
+//
+//        final Canvas canvas = new Canvas(W, H);
+//        AnimationTimer timer = new AnimationTimer() {
+//            @Override
+//            public void handle(long now) {
+//                GraphicsContext gc = canvas.getGraphicsContext2D();
+//                gc.setFill(Color.CORNSILK);
+//                gc.fillRect(0, 0, W, H);
+//                gc.setFill(Color.FORESTGREEN);
+//                gc.fillOval(
+//                        x.doubleValue(),
+//                        y.doubleValue(),
+//                        D,
+//                        D
+//                );
+//            }
+//        };
+//
+//        stage.setScene(
+//                new Scene(
+//                        new Group(
+//                                canvas
+//                        )
+//                )
+//        );
+//        stage.show();
+//
+//        timer.start();
+//        timeline.play();
+//    }
 
 
     public static void main(String[] args) {
--- src/sample/SwapLevel.java
+++ src/sample/SwapLevel.java
@@ -6,25 +6,58 @@ import java.util.Random;
 public class SwapLevel {
 
     private Tiles[][] tileGrid;
+    private Tiles[][] oldGrid;
     private Tiles[] previewRow;
     private int power;
     private int acceleration;
     private int points;
     private int level;
     private int goal;
+    private int moves;
     private Tiles[] randTable;
+    private boolean running;
 
 
-    public SwapLevel(int level, int goal) {
+    public SwapLevel(int level, int goal, int moves) {
         tileGrid = new Tiles[8][8];
+        oldGrid = new Tiles[8][8];
         previewRow = new Tiles[tileGrid.length];
         power = 0;
-        points = goal/3;
+        points = 0;
         acceleration = 0;
         this.level = level;
         this.goal = goal;
+        running = false;
+        this.moves = moves;
         genRandArray();
         initialize();
+        copyGrid();
+        running = true;
+//        printGrid();
+    }
+
+    private void copyGrid() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                oldGrid[i][j] = tileGrid[i][j];
+            }
+        }
+    }
+
+    private void revert() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                tileGrid[i][j] = oldGrid[i][j];
+            }
+        }
+    }
+
+    private void printGrid() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.println(tileGrid[i][j].toString());
+            }
+        }
     }
 
     private void genRandArray() {
@@ -33,7 +66,7 @@ public class SwapLevel {
         for (Tiles tile : tiles) {
             for (int i = 0; i < tile.getWeight(); i++) {
                 randArray.add(tile);
-                System.out.println(tile.toString());
+//                System.out.println(tile.toString());
             }
         }
         randTable = new Tiles[randArray.size()];
@@ -43,34 +76,207 @@ public class SwapLevel {
     }
 
     public void simulate() {
+        boolean changed = false;
+        while (elimMatches()) {
+            changed = true;
+            collapse();
+            refill();
+        }
+        if (!changed)
+            revert();
+        else {
+            if (running)
+                moves--;
+        }
 
+        copyGrid();
     }
 
-    public void elimMatches() {
+    public void refill() {
+        boolean filled = false;
+        while (!filled) {
+            filled = true;
+            dropRow();
+            for (Tiles tile : previewRow) {
+                if (tile == Tiles.blank)
+                    filled = false;
+            }
+            fillPreview();
+        }
+    }
+
+    private void fillPreview() {
+        Random r = new Random();
+        for (int i = 0; i < tileGrid.length; i++) {
+            if (previewRow[i] == Tiles.blank || previewRow[i] == null)
+                previewRow[i] = randTable[r.nextInt(randTable.length)];
+        }
+    }
+
+    public void dropRow() {
+        for (int i = 0; i < 8; i++) {
+            int j = -1;
+            while (j < 7 && tileGrid[i][j+1] == Tiles.blank) {
+                j++;
+            }
+            if (j >= 0) {
+                tileGrid[i][j] = previewRow[i];
+                previewRow[i] = Tiles.blank;
+            }
+        }
+    }
+
+    public void collapse() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 7; j >= 0; j--) {
+                tileGrid[i][j] = getLowTile(i,j);
+            }
+        }
+    }
+
+    public Tiles getLowTile(int x, int y) {
+        if (y < 0)
+            return Tiles.blank;
+        else if (tileGrid[x][y] != Tiles.blank) {
+            Tiles tile = tileGrid[x][y];
+            tileGrid[x][y] = Tiles.blank;
+            return tile;
+        } else
+            return getLowTile(x, y-1);
+    }
+
+    private void score(ArrayList<Tiles> match) {
+        if (running) {
+            for (Tiles tile : match)
+                System.out.println(tile.toString());
+            System.out.println();
+            int score = 0;
+            if (Tiles.basic(match.get(0)) == Tiles.orange || Tiles.basic(match.get(1)) == Tiles.cyan || Tiles.basic(match.get(1)) == Tiles.red || Tiles.basic(match.get(1)) == Tiles.green) {
+                score = match.size() * GameStats.baseValue;
+                if (match.size() - 3 > 0)
+                    score *= Math.pow(GameStats.bigMatchMult, match.size() - 2);
+                for (Tiles tile : match) {
+                    if (Tiles.basic(tile) != tile)
+                        score *= GameStats.superMult;
+                }
+                points += score * (1 + acceleration / 100.0);
+            } else {
+                switch (match.get(0)) {
+                    case power:
+                        power += match.size() * GameStats.powerBaseVal * Math.pow(GameStats.bigMatchMult, Math.max(0, match.size() - 3));
+                        break;
+                    case accelerate:
+                        acceleration += match.size() * GameStats.accBaseVal * Math.pow(GameStats.bigMatchMult, Math.max(0, match.size() - 3));
+                        break;
+                    case decelerate:
+                        points -= match.size() * GameStats.accBaseVal * Math.pow(GameStats.bigMatchMult, Math.max(0, match.size() - 3)) * 3;
+                        acceleration -= match.size() * GameStats.accBaseVal * Math.pow(GameStats.bigMatchMult, Math.max(0, match.size() - 3)) * 2;
+                        break;
+                    case extraMove:
+                        moves += Math.pow(2, match.size() - 3);
+                        break;
+                }
+                acceleration = Math.max(acceleration, -50);
+            }
+        }
+    }
+
+    public boolean elimMatches() {
+        Random r = new Random();
+        boolean changed = false;
         for (int i = 0; i < 8; i++) {
             int lastSamex = 0;
             Tiles lastSame = null;
             for (int j = 0; j < 8; j++) {
-                if (tileGrid[j][i] != lastSame) {
+                if (Tiles.basic(tileGrid[j][i]) != lastSame) {
                     if (j - lastSamex > 2) {
-
+                        ArrayList<Tiles> match = new ArrayList<Tiles>();
+                        changed = true;
+                        for (int k = lastSamex; k < j; k++) {
+                            match.add(tileGrid[k][i]);
+                            tileGrid[k][i] = Tiles.blank;
+                        }
+                        score(match);
+                        if (j - lastSamex > 4) {
+                            if (r.nextDouble() < GameStats.match5super && lastSame.getMulti() != null) {
+                                tileGrid[lastSamex+2][i] = lastSame.getMulti();
+                            }
+                        } else if (j - lastSamex > 3) {
+                            if (r.nextDouble() < GameStats.match4super && lastSame.getMulti() != null) {
+                                tileGrid[lastSamex+2][i] = lastSame.getMulti();
+                            }
+                        }
                     }
-                    lastSame = tileGrid[j][i];
+                    lastSame = Tiles.basic(tileGrid[j][i]);
                     lastSamex = j;
                 }
             }
-        }
-    }
-
-    public void initRand() {
-        Random r = new Random();
-        Tiles[] tiles = Tiles.values();
-        for (int i = 0; i < tileGrid.length; i++) {
-            previewRow[i] = tiles[r.nextInt(tiles.length)];
-            for (int j = 0; j < tileGrid.length; j++) {
-                tileGrid[i][j] = tiles[r.nextInt(tiles.length)];
+            if (8 - lastSamex > 2) {
+                changed = true;
+                ArrayList<Tiles> match = new ArrayList<Tiles>();
+                for (int k = lastSamex; k < 8; k++) {
+                    match.add(tileGrid[k][i]);
+                    tileGrid[k][i] = Tiles.blank;
+                }
+                score(match);
+                if (8 - lastSamex > 4) {
+                    if (r.nextDouble() < GameStats.match5super && lastSame.getMulti() != null) {
+                        tileGrid[lastSamex+2][i] = lastSame.getMulti();
+                    }
+                } else if (8 - lastSamex > 3) {
+                    if (r.nextDouble() < GameStats.match4super && lastSame.getMulti() != null) {
+                        tileGrid[lastSamex+2][i] = lastSame.getMulti();
+                    }
+                }
             }
         }
+        for (int i = 0; i < 8; i++) {
+            int lastSamey = 0;
+            Tiles lastSame = null;
+            for (int j = 0; j < 8; j++) {
+                if (Tiles.basic(tileGrid[i][j]) != lastSame) {
+                    if (j - lastSamey > 2) {
+                        ArrayList<Tiles> match = new ArrayList<Tiles>();
+                        changed = true;
+                        for (int k = lastSamey; k < j; k++) {
+                            match.add(tileGrid[i][k]);
+                            tileGrid[i][k] = Tiles.blank;
+                        }
+                        score(match);
+                        if (j - lastSamey > 4) {
+                            if (r.nextDouble() < GameStats.match5super && lastSame.getMulti() != null) {
+                                tileGrid[i][lastSamey+2] = lastSame.getMulti();
+                            }
+                        } else if (j - lastSamey > 3) {
+                            if (r.nextDouble() < GameStats.match4super && lastSame.getMulti() != null) {
+                                tileGrid[i][lastSamey+2] = lastSame.getMulti();
+                            }
+                        }
+                    }
+                    lastSame = Tiles.basic(tileGrid[i][j]);
+                    lastSamey = j;
+                }
+            }
+            if (8 - lastSamey > 2) {
+                ArrayList<Tiles> match = new ArrayList<Tiles>();
+                changed = true;
+                for (int k = lastSamey; k < 8; k++) {
+                    match.add(tileGrid[i][k]);
+                    tileGrid[i][k] = Tiles.blank;
+                }
+                score(match);
+                if (8 - lastSamey > 4) {
+                    if (r.nextDouble() < GameStats.match5super && lastSame.getMulti() != null) {
+                        tileGrid[i][lastSamey+2] = lastSame.getMulti();
+                    }
+                } else if (8 - lastSamey > 3) {
+                    if (r.nextDouble() < GameStats.match4super && lastSame.getMulti() != null) {
+                        tileGrid[i][lastSamey+2] = lastSame.getMulti();
+                    }
+                }
+            }
+        }
+        return changed;
     }
 
     public void initialize() {
@@ -82,6 +288,9 @@ public class SwapLevel {
                 tileGrid[i][j] = randTable[r.nextInt(randTable.length)];
             }
         }
+        printGrid();
+        simulate();
+        printGrid();
     }
 
     public Tiles[][] getTileGrid() {
@@ -90,6 +299,10 @@ public class SwapLevel {
 
     public Tiles[] getPreviewRow() {
         return previewRow;
+    }
+
+    public int getMoves() {
+        return moves;
     }
 
     public int getPower() {
