bb4ca79cf9483a23a319372924c2b2d5faa7abd9
AlexAndrew28 <60033885+AlexAndrew28@users.noreply.github.com>
2020-11-15 12:35:52 +0000

    added even moar stuff fixed some tings hopeful

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,10 +11,13 @@ import javafx.stage.Stage;
 import java.awt.event.MouseEvent;
 
 public class Main extends Application {
-
+    Stage primaryStage;
+    Scene mainMenu;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+        this.primaryStage = primaryStage;
         primaryStage.setTitle("Time Swap");
 
         primaryStage.setMaximized(true);
@@ -22,15 +25,11 @@ public class Main extends Application {
         Button button_endless = new Button("Endless Mode");
         Button button_campaign = new Button("Campaign mode");
         Button button_inventory = new Button("Inventory");
-        Button button_shop = new Button("Shop");
         Button button_test = new Button("Test");
         
         //System.out.println("HI ALEX");
         //System.out.println("sup bitches");
 
-        button_shop.setOnAction(value ->  {
-            System.out.println("button - shop");
-        });
 
 
 
@@ -39,7 +38,6 @@ public class Main extends Application {
         mmgp.add(button_endless, 0, 0, 1, 1);
         mmgp.add(button_campaign, 0, 1, 1, 1);
         mmgp.add(button_inventory, 0, 2, 1, 1);
-        mmgp.add(button_shop, 0, 3, 1, 1);
 
         mmgp.add(button_test, 0, 4, 1, 1);
 
@@ -70,14 +68,6 @@ public class Main extends Application {
             primaryStage.show();
         });
 
-        button_shop.setOnAction(value -> {
-            try {
-                primaryStage.setScene(shopScene);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            primaryStage.show();
-        });
 
         button_inventory.setOnAction(value -> {
             try {
@@ -90,12 +80,20 @@ public class Main extends Application {
 
         button_endless.setOnAction(value -> {
             try {
-                primaryStage.setScene(new GameScreen().getScene());
+                primaryStage.setScene(new GameScreen(this, is.getValueOfInvSlots(), 1, 10000,20).getScene());
             } catch (Exception e) {
                 e.printStackTrace();
             }
             primaryStage.show();
         });
+    }
+
+    public void endGame(int points, int goal, int movesLeft) {
+        primaryStage.setScene(new LevelEnd(points, goal, movesLeft, this).getScene());
+    }
+
+    public void loadMainMenu() {
+        primaryStage.setScene(mainMenu);
     }
 
     public static void main(String[] args) {
